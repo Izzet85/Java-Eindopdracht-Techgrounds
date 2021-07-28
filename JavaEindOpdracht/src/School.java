dcdb8cea920856a297c6f1b52215e3639347d6cc
Izzet85 <70436250+Izzet85@users.noreply.github.com>
2021-07-28 11:49:58 +0200

    Add files via upload
    update commit

---
+++ JavaEindOpdracht/src/School.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class School {
+    ArrayList <String> teachers = new ArrayList<String>();
+    ArrayList<String> students = new ArrayList<String>();
+    ArrayList<String> classes = new ArrayList<String>();
+    String schoolType;
+    ArrayList<String> curriculum = new ArrayList<String>();
+
+    public ArrayList<String> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(ArrayList<String> teachers) {
+        this.teachers = teachers;
+    }
+
+    public ArrayList<String> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<String> students) {
+        this.students = students;
+    }
+
+    public ArrayList<String> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(ArrayList<String> classes) {
+        this.classes = classes;
+    }
+
+    public String getSchoolType() {
+        return schoolType;
+    }
+
+    public void setSchoolType(String schoolType) {
+        this.schoolType = schoolType;
+    }
+
+    public ArrayList<String> getCurriculum() {
+        return curriculum;
+    }
+
+    public void setCurriculum(ArrayList<String> curriculum) {
+        this.curriculum = curriculum;
+    }
+
+    public void addStudent(){
+
+
+    }
+
+    public void addTeacher(){
+
+
+    }
+
+}
